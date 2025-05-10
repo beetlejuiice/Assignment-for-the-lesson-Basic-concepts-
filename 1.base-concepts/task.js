d5c2974cf6bb1c9d9fcae74032c7295a3497b579
Anastasia <anastasiatrushnikova@mail.ru>
2025-05-10 20:11:40 +0500

    Реализовано первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,22 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
+  let discriminant = b**2 - 4*a*c;
+
+  if (discriminant < 0) {
+    return arr;
+  } else if (discriminant === 0) {
+    let root = -b / (2*a);
+    arr.push(root);
+    return arr;
+    } else {
+    let root1 = (-b + Math.sqrt(discriminant)) / (2*a);
+    let root2 = (-b - Math.sqrt(discriminant)) / (2*a);
+    arr.push(root1);
+    arr.push(root2);
+    return arr;
+      }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
