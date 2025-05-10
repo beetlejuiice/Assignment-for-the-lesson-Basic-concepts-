768f8125dbd1c0a92643868f849bea266ef90083
Anastasia <anastasiatrushnikova@mail.ru>
2025-05-10 20:48:53 +0500

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,24 +1,31 @@
-"use strict";
-
 function solveEquation(a, b, c) {
-  let arr = [];
-  let discriminant = b**2 - 4*a*c;
+	let arr = [];
+	let discriminant = b ** 2 - 4 * a * c;
 
-  if (discriminant < 0) {
-    return arr;
-  } else if (discriminant === 0) {
-    let root = -b / (2*a);
-    arr.push(root);
-    return arr;
-    } else {
-    let root1 = (-b + Math.sqrt(discriminant)) / (2*a);
-    let root2 = (-b - Math.sqrt(discriminant)) / (2*a);
-    arr.push(root1);
-    arr.push(root2);
-    return arr;
-      }
+	if (discriminant < 0) {
+		return arr;
+	} else if (discriminant === 0) {
+		let root = -b / (2 * a);
+		arr.push(root);
+		return arr;
+	} else {
+		let root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+		let root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+		arr.push(root1);
+		arr.push(root2);
+		return arr;
+	}
 }
+solveEquation(1, 0, -1);
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	if (amount <= contribution || percent < 0 || countMonths <= 0) {
+		return 0;
+	}
+	let monthlyPercent = (percent / 100) / 12;
+	let creditBody = amount - contribution;
+	let monthlyPayment = creditBody * (monthlyPercent + (monthlyPercent / ((Math.pow(1 + monthlyPercent, countMonths) - 1))));
+	let totalAmount = monthlyPayment * countMonths + contribution;
+	return parseFloat(totalAmount.toFixed(2));
 }
+console.log(calculateTotalMortgage(6, 21, 5000000, 240));
