4d4d8c0899457b5b47ae1c1b32870bc67bc3a455
Anastasia <anastasiatrushnikova@mail.ru>
2025-05-11 16:21:24 +0500

    Изменения в первом задании

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,18 +3,16 @@ function solveEquation(a, b, c) {
 	let discriminant = b ** 2 - 4 * a * c;
 
 	if (discriminant < 0) {
-		return arr;
 	} else if (discriminant === 0) {
 		let root = -b / (2 * a);
 		arr.push(root);
-		return arr;
 	} else {
 		let root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
 		let root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
 		arr.push(root1);
 		arr.push(root2);
-		return arr;
 	}
+	return arr;
 }
 solveEquation(1, 0, -1);
 
@@ -25,7 +23,7 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
     const monthlyPercent = (percent / 100) / 12;
     const creditBody = amount - contribution;
     const monthlyPayment = (creditBody * monthlyPercent) / (1 - Math.pow(1 + monthlyPercent, -countMonths));
-    const totalAmount = monthlyPayment * countMonths + contribution;
+    const totalAmount = monthlyPayment * countMonths;
     return parseFloat(totalAmount.toFixed(2));
 }
 console.log(calculateTotalMortgage(6, 21, 5000000, 240));
